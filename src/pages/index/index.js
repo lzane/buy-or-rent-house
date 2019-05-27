c04079ee607638ee4122a53bdbaf618b1d148eaf
zane <cn.zanelee@gmail.com>
2019-05-27 13:53:17 +0800

    [+] add fv function

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -8,6 +8,25 @@ import './index.scss'
 let finance = new Finance();
 const downPaymentPercentRange = [3, 5, 7];
 
+/**
+ * @return {string}
+ */
+function FV(rate, nper, pmt, pv, type) {
+  if (!type) type = 0;
+
+  var pow = Math.pow(1 + rate, nper);
+  var fv = 0;
+
+  if (rate) {
+    fv = (pmt * (1 + rate * type) * (1 - pow) / rate) - pv * pow;
+  } else {
+    fv = -1 * (pv + pmt * nper);
+  }
+
+  return fv;
+}
+
+
 export default class Index extends Component {
 
   config = {
@@ -18,11 +37,11 @@ export default class Index extends Component {
     super(...arguments);
     this.state = {
       housePrice: 400,
-      downPaymentPercent: 3,
+      loan: 251,
       loanYears: 30,
       loanRate: 5.15,
-      extraDownPayment: 0,
-      houseIncreaseRate: 5,
+      downPayment: 160,
+      houseIncreaseRate: 3,
       rentPrice: 0.6,
       rentIncreaseRate: 5,
       financeCost: 5,
@@ -38,18 +57,18 @@ export default class Index extends Component {
     return value
   }
 
-  handlePickerChange(item, value) {
-    let result;
-    if (item === 'downPaymentPercent') {
-      result = downPaymentPercentRange[value.detail.value];
-    }
-    console.log(item, result, value, value.detail);
-    this.setState({
-      [item]: result
-    });
-
-    return value
-  }
+  // handlePickerChange(item, value) {
+  //   let result;
+  //   if (item === 'downPaymentPercent') {
+  //     result = downPaymentPercentRange[value.detail.value];
+  //   }
+  //   console.log(item, result, value, value.detail);
+  //   this.setState({
+  //     [item]: result
+  //   });
+  //
+  //   return value
+  // }
 
 
   componentWillMount() {
@@ -68,9 +87,16 @@ export default class Index extends Component {
   }
 
   render() {
-    const {housePrice, downPaymentPercent, extraDownPayment, loanRate, loanYears} = this.state;
-    const totalDownPay = housePrice * downPaymentPercent / 10 + extraDownPayment;
-    const pmt = finance.PMT(loanRate / 100 / 12, loanYears * 12, housePrice * (1 - downPaymentPercent / 10))
+    const {loanRate, loanYears, loan, houseIncreaseRate,housePrice} = this.state;
+    const pmt = parseFloat(finance.PMT(loanRate / 100 / 12, loanYears * 12, loan).toFixed(4));
+    const months = 20*12;
+    // const housePriceAfterYears = finance.FV(this.state.houseIncreaseRate,)
+
+    const loanAfterMonths = FV(loanRate/100/12,months,pmt,loan);
+    const housePriceAfterMonths = FV(houseIncreaseRate/100/12,months,0,-housePrice);
+
+    const netAssetRentingHouse = loanAfterMonths;
+    const netAssetBuyingHouse = housePriceAfterMonths;
 
     return (
       <View className='index'>
@@ -83,30 +109,37 @@ export default class Index extends Component {
             value={this.state.housePrice}
             onChange={this.handleChange.bind(this, 'housePrice')}
           />
-          <Picker
-            mode='selector' range={downPaymentPercentRange}
-            onChange={this.handlePickerChange.bind(this, 'downPaymentPercent')}
-            className='at-input'
-          >
-            <View className='at-input__container'>
-              <Text className='picker at-input__title'>
-                首付成数
-              </Text>
-              <Text className='at-input__input'> {this.state.downPaymentPercent} </Text>
-              <View className='at-input__children'><Text className='taro-text'> 成 </Text>
-              </View>
-            </View>
-          </Picker>
           <AtInput
-            name='loanYears'
-            title='贷款年数'
-            type='number'
-            placeholder='请输入数字'
-            value={this.state.loanYears}
-            onChange={this.handleChange.bind(this, 'loanYears')}
+            name='downPayment'
+            title='首付+额外支出'
+            type='digit'
+            placeholder='包含税费，中介费等。请输入数字'
+            value={this.state.downPayment}
+            onChange={this.handleChange.bind(this, 'downPayment')}
           >
-            <Text> 年 </Text>
           </AtInput>
+          <AtInput
+            name='housePrice'
+            title='贷款总额'
+            type='digit'
+            placeholder='请输入数字'
+            value={this.state.loan}
+            onChange={this.handleChange.bind(this, 'loan')}
+          />
+          {/*<Picker*/}
+          {/*  mode='selector' range={downPaymentPercentRange}*/}
+          {/*  onChange={this.handlePickerChange.bind(this, 'downPaymentPercent')}*/}
+          {/*  className='at-input'*/}
+          {/*>*/}
+          {/*  <View className='at-input__container'>*/}
+          {/*    <Text className='picker at-input__title'>*/}
+          {/*      首付成数*/}
+          {/*    </Text>*/}
+          {/*    <Text className='at-input__input'> {this.state.downPaymentPercent} </Text>*/}
+          {/*    <View className='at-input__children'><Text className='taro-text'> 成 </Text>*/}
+          {/*    </View>*/}
+          {/*  </View>*/}
+          {/*</Picker>*/}
           <AtInput
             name='loanRate'
             title='房贷利率'
@@ -117,17 +150,16 @@ export default class Index extends Component {
           >
             <Text> % </Text>
           </AtInput>
-
           <AtInput
-            name='extraDownPayment'
-            title='首付额外支出'
-            type='digit'
-            placeholder='税费，中介费等。请输入数字'
-            value={this.state.extraDownPayment}
-            onChange={this.handleChange.bind(this, 'extraDownPayment')}
+            name='loanYears'
+            title='贷款年数'
+            type='number'
+            placeholder='请输入数字'
+            value={this.state.loanYears}
+            onChange={this.handleChange.bind(this, 'loanYears')}
           >
+            <Text> 年 </Text>
           </AtInput>
-
 
           <AtInput
             name='houseIncreaseRate'
@@ -173,9 +205,9 @@ export default class Index extends Component {
         </AtForm>
 
         <AtButton type='primary'>点击计算</AtButton>
-
-        <Text> 所有首付： {totalDownPay} </Text>
-        <Text> 每月供款: {pmt} </Text>
+        <View> 每月供款: {pmt} </View>
+        <View> 买房10年净资产： {netAssetBuyingHouse} </View>
+        <View> 租房10年净资产： {netAssetRentingHouse} </View>
       </View>
     )
   }
